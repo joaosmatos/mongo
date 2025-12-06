46b9ee790ab86e4d981f9e246c35b9937ce6fff4
joaosmatos <joaovsmatoss@gmail.com>
2025-12-06 00:44:56 -0300

    trabalhando com mongoose

--- aprendendo.js
+++ aprendendo.js
@@ -9,3 +9,45 @@ const mongoose = require("mongoose")
     }).catch((err) => {
         console.log("Houve um erro ao se conectar ao mongoDB: "+err)
     })
+// model - usuarios
+// definindo o model
+
+    const UsuarioSchema = mongoose.Schema({
+
+        nome: {
+            type: String,
+            require: true
+        },
+        sobrenome: {
+            type: String,
+            require: true
+        },
+        email: {
+            type: String,
+            require: true
+        },
+        idade: {
+            type: Number,
+            require: true
+        },
+        pais: {
+            type: string
+        }
+
+    })
+// collection
+    mongoose.model('usuarios', UsuarioSchema )
+
+    const victor = mongoose.model('usuarios')
+
+    new victor({
+        nome: "victor",
+        sobrenome: "lima",
+        email: "email@email.com",
+        idade: 19,
+        pais: "Brasil"
+    }).save().then(() => {
+        console.log("usuario criado com sucesso!")
+    }).catch((err) => {
+        console.log("Houve um erro ao registrar o usuario: "+err)
+    })
